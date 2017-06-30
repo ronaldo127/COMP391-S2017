466c843dbf4fe629febf22c732e443e874a611bf
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2017-06-30 11:22:35 -0400

    Explosion animation.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Boundery")) return;
+        
+        Destroy(other.gameObject);
+        animator.SetTrigger("Explode");
+    }
+
+    public void DestroyMe()
+    {
+        Destroy(gameObject);
+    }
+
+}
--- Assets/Scripts/DestroyLasers.cs
+++ Assets/Scripts/DestroyLasers.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class DestroyLasers : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     void OnTriggerExit2D(Collider2D collider) {
         Destroy(collider.gameObject);
     }
